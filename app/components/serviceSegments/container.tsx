23a05022f33d48e7e77f202f872bd17a65488ce4
marisanest <marisanest@mailbox.org>
2024-11-18 23:29:29 +0100

    ...

--- app/components/serviceSegments/container.tsx
+++ app/components/serviceSegments/container.tsx
@@ -11,7 +11,7 @@ export default function ServiceSegmentsContainer({children}: ReactNodeProps) {
     return (
         <div className="relative z-10 w-full flex justify-center px-x-sm sm:px-0">
             <div className={cn(
-                "w-full sm:w-sm sm:max-w-sm px-x-s s:px-x-sm py-y-sm bg-orange-200 rounded-tr-md shadow-[0px_0px_0px_0px_black] transition-[border-radius] duration-1000",
+                "w-full max-w-sm px-x-s s:px-x-sm py-y-sm bg-orange-200 rounded-tr-md shadow-[0px_0px_0px_0px_black] transition-[border-radius] duration-1000",
                 showServiceSegmentDetails ? "rounded-bl-md !shadow-[0px_6px_10px_-10px_black]" : 'shadow-[0px_0px_0px_0px_black]'
             )}>
                 {children}
