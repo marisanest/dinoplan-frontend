8585945697e4a800e7cb6c5443b26ad0c4ca9001
marisanest <marisanest@mailbox.org>
2025-02-21 17:54:47 +0100

    ...

--- app/lib/signIn.ts
+++ app/lib/signIn.ts
@@ -7,29 +7,17 @@ export async function signIn(
     prevState: any,
     formData: FormData = new FormData(),
 ) {
-    // Verify the passphrase
-
     const { HASHED_PASSPHRASE } = process.env;
     const inputPassword = formData.get("password");
 
-    // if (!compareSync('dinostark' as string, '\\$2a\\$08\\$hdcXWBF7GbW5M3YIp3FrZOEwl9xjIaaJ6EQoTEi1b8ejp2dkeb/iS' as string)) {
-    //     return {
-    //         error: "Falsches Passwort",
-    //     };
-    // }
+    if (!compareSync(inputPassword, HASHED_PASSPHRASE)) {
+        return {
+            error: "Falsches Passwort",
+        };
+    }
 
     const auth = getAuth();
     await auth.set("hasAccess", true);
-    return { success: true };
-}
-
-export async function signOut(
-    prevState: any,
-    formData: FormData = new FormData(),
-) {
-    // Destroy the session
-    const auth = getAuth();
-    await auth.clear();
     return { success: true };
 }
 
