68232bb1af62484acf53c0c0f69fbaf826d92b46
marisanest <marisanest@mailbox.org>
2025-05-21 12:07:58 +0200

    calendar

--- app/rechner/berechnung/components/ctas/appointment/embed.tsx
+++ app/rechner/berechnung/components/ctas/appointment/embed.tsx
@@ -1,5 +1,4 @@
 import React, {useState} from "react";
-import Calendly from "@/components/calendly";
 
 export default function CalculatorCalculationCtasAppointmentEmbed({
                                                                       baseUrl,
@@ -13,8 +12,12 @@ export default function CalculatorCalculationCtasAppointmentEmbed({
     return (
         <div className="transition-[height] duration-700 overflow-y-hidden" style={{height: isAppointmentEmbedVisible ? `${height}px` : '0px'}}>
             <div ref={el => setHeight(el?.clientHeight ?? 0)}>
-                <Calendly className="h-[calc(775px+var(--spacing-y-xs))] mt-y-xs w-[calc(100dvw-2*var(--spacing-x-outer)-2*var(--spacing-x-xxs))] min-w-[calc(100dvw-2*var(--spacing-x-outer)-2*var(--spacing-x-xxs))] max-w-[calc(100dvw-2*var(--spacing-x-outer)-2*var(--spacing-x-xxs))] ss:min-w-[460px] ss:w-[460px] ss:max-w-[460px]"
-                          baseUrl={baseUrl}/>
+              <div className="flex mt-y-xs">
+                <iframe className="w-[calc(375px-48px)] min-w-[calc(375px-48px)] max-w-[calc(375px-48px)] h-[2219px] xs:w-[420px] xs:min-w-[420px] xs:max-w-[420px] xs:h-[2248px]"
+                        src='https://outlook.office.com/owa/calendar/Dinoplan@finanzhebel.de/bookings/?ismsaljsauthenabled'
+                        height='100%'
+                />
+              </div>
             </div>
         </div>
     );
