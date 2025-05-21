5dfd2451bb6eacd38ed7b60cc54e909b497be6e3
marisanest <marisanest@mailbox.org>
2025-05-21 11:54:25 +0200

    calendar

--- app/kalender/components/calendar.tsx
+++ app/kalender/components/calendar.tsx
@@ -1,13 +1,13 @@
 "use client"
 
-import Calendly from "@/components/calendly";
-
-export default function Calendar({contact}: {contact: any}) {
+export default function Calendar() {
     return (
-        <main className="min-w-[100dvw] w-[100dvw] max-w-[100dvw] flex justify-center pt-banner bg-orange-400">
+        <main className="min-w-[100dvw] w-[100dvw] max-w-[100dvw] min-h-[100dvh] flex justify-center pt-banner bg-orange-400">
             <div className="flex pt-y-m">
-                <Calendly className="w-[calc(100dvw-2*var(--spacing-x-outer))] min-w-[calc(100dvw-2*var(--spacing-x-outer))] max-w-[calc(100dvw-2*var(--spacing-x-outer))] min-h-[775px] ss:min-w-[460px] ss:w-[460px] ss:max-w-[460px]"
-                          baseUrl={contact.calendly.url}/>
+              <iframe className="w-[375px] min-w-[375px] max-w-[375px] h-[2268px] xs:w-[420px] xs:min-w-[420px] xs:max-w-[420px] xs:h-[2248px]"
+                      src='https://outlook.office.com/owa/calendar/Dinoplan@finanzhebel.de/bookings/?ismsaljsauthenabled'
+                      height='100%'
+              />
             </div>
         </main>
     );
--- app/kalender/page.tsx
+++ app/kalender/page.tsx
@@ -1,10 +1,6 @@
 import Calendar from "@/kalender/components/calendar";
-import {sanityClient} from "@/lib/sanity/client";
 import type {Metadata} from "next";
 import {getMetadata} from "@/lib/metadata";
-import {CONTACT_QUERY} from "@/lib/sanity/queries/contact";
-
-const options = { next: { revalidate: 3600 } };
 
 export const metadata: Metadata = getMetadata(
     "Dinoplan | Kalender",
@@ -12,7 +8,5 @@ export const metadata: Metadata = getMetadata(
 );
 
 export default async function LandingPage() {
-  const contact = await sanityClient.fetch(CONTACT_QUERY, {}, options);
-
-  return <Calendar contact={contact}/>;
+  return <Calendar />;
 }
